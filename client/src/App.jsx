e6a2be54ae8232ba7c5bbc53cbee404132e50a40
lipika1911 <lipikaarya1723@gmail.com>
2025-07-09 13:55:34 +0530

    feat: add react-router-dom for routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
-import Home from './pages/Home/Home'
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import SignUp from './pages/SignUp/SignUp';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/dashboard",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+]);
 
 const App = () => {
-  return (
-    <div>
-      <Home />
-    </div>
-  )
-}
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
