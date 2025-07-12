233de8624fe6ef74a0e864449e7f15548016fb5e
lipika1911 <lipikaarya1723@gmail.com>
2025-07-12 10:42:31 +0530

    feat: implement user registration endpoint with validation and use .env for MongoDB connection string

--- server/index.js
+++ server/index.js
@@ -8,8 +8,11 @@ const cors = require("cors");
 const app = express();
 const PORT = 8000;
 
-// ✅ Simplified MongoDB connection
-mongoose.connect(config.connectionString)
+const jwt = require("jsonwebtoken");
+const {authenticateToken} = require('./utilities');
+
+
+mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log("✅ MongoDB connected successfully");
     })
@@ -17,11 +20,61 @@ mongoose.connect(config.connectionString)
         console.error("❌ MongoDB connection error:", err);
     });
 
+const User = require("./models/user.model");
+
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 
 app.get("/", (req, res) => {
     res.json({ data: "hello" });
+});
+
+app.post("/create-account", async(req, res) => {
+    const { fullName, email, password } = req.body;
+
+    if(!fullName){
+        return res
+            .status(400)
+            .json({error: true, message: 'Full Name is required'})
+    }
+
+    if(!email){
+        return res.status(400).json({error: true, message: "Email is required"});
+    }
+
+    if(!password){
+        return res
+            .status(400)
+            .json({error: true, message: "Password is required"}); 
+    }
+
+    const isUser = await User.findOne({email: email});
+
+    if(isUser){
+        return res.json({
+            error: true,
+            message: "user already exists!",
+        })
+    }
+
+    const user = new User({
+        fullName, 
+        email, 
+        password,
+    });
+
+    await user.save();
+
+    const accessToken = jwt.sign({user}, process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn: "3600m"
+    })
+
+    return res.json({
+        error: false,
+        user,
+        accessToken,
+        message: "Registration Succesful",
+    })
 });
 
 app.listen(PORT, () => {
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -8,4 +8,4 @@ const userSchema = new Schema ({
     createdOn: {type: Date, default: new Date().getTime() },
 });
 
-module.exports = mongooose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
