f24c98d0acada2356570672df856a82da1890a97
lipika1911 <lipikaarya1723@gmail.com>
2025-07-12 19:40:57 +0530

    feat: add edit-note endpoint with validation and error handling

--- server/index.js
+++ server/index.js
@@ -117,6 +117,7 @@ app.post("/login", async(req, res) => {
     }
 });
 
+//ADD NOTE
 app.post("/add-note", authenticateToken, async(req,res) => {
     const {title, content, tags} = req.body;
     const {user} = req.user;
@@ -152,6 +153,46 @@ app.post("/add-note", authenticateToken, async(req,res) => {
     }
     
 })
+
+
+//EDIT NOTE
+app.put("/edit-note/:noteId", authenticateToken, async(req,res) => {
+    const noteId = req.params.noteId;
+    const {title, content, tags, isPinned} = req.body;
+    const {user} = req.user;
+
+    if(!title && !content && !tags){
+        return res.status(400).json({error: true, message: "No changed Provided"});
+    }
+
+    try{
+        const note = await Note.findOne({ _id: noteId, userId: user._id});
+
+        if(!note){
+            return res.status(404).json({error: true, message: "Note not found"});
+        }
+
+        if(title) note.title = title;
+        if(content) note.content = content;
+        if(tags) note.tags = tags;
+        if(isPinned) note.isPinned = isPinned;
+
+        await note.save();
+
+        return res.json({
+            error: false,
+            note,
+            message: "Note updated successfully"
+        });
+    }catch(error){
+        return res.status(500).json({
+            error: true,
+            message: "Internal Server Error"
+        });
+    }
+})
+
+
 
 app.listen(PORT, () => {
     console.log(`🚀 Server is running on http://localhost:${PORT}`);
