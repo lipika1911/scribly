684bb0c3e603acfb744fcf0585457b8d1e96568b
lipika1911 <lipikaarya1723@gmail.com>
2025-07-12 19:19:21 +0530

    feat: implement add-note api endpoint

--- server/index.js
+++ server/index.js
@@ -21,6 +21,7 @@ mongoose.connect(process.env.MONGO_URI)
     });
 
 const User = require("./models/user.model");
+const Note = require("./models/note.model");
 
 app.use(express.json());
 app.use(cors({ origin: "*" }));
@@ -115,6 +116,42 @@ app.post("/login", async(req, res) => {
         });
     }
 });
+
+app.post("/add-note", authenticateToken, async(req,res) => {
+    const {title, content, tags} = req.body;
+    const {user} = req.user;
+
+    if(!title){
+        return res.status(400).json({error: true, message: "Title is required"});
+    }
+
+    if(!content){
+        return res.status(400).json({error: true, message: "Content is required"});
+    }
+
+    try{
+        const note = new Note({
+            title,
+            content, 
+            tags: tags || [],
+            userId: user._id,
+        })
+
+        await note.save();
+
+        return res.json({
+            error: false,
+            note,
+            message: "Note added successfully"
+        })
+    }catch(error){
+        return res.status(500).json({
+            error: true,
+            message: "Internal server error",
+        });
+    }
+    
+})
 
 app.listen(PORT, () => {
     console.log(`🚀 Server is running on http://localhost:${PORT}`);
